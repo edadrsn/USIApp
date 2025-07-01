2651a83fa794cadcc0ca97ff3e8e063b046233ef
Eda Dursun <edadrsn929@gmail.com>
2025-07-01 09:34:27 +0300

    xml design updated

--- android/app/src/main/java/com/example/usiapp/MainActivity.kt
+++ android/app/src/main/java/com/example/usiapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.usiapp
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,4 +19,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
     }
+
+
+    fun clickIndustry(view: View){
+
+    }
+
+
+    fun clickAcademician(view:View){
+
+    }
+
+
 }
