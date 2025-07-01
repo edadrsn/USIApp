44dd818c94c526fb9feac621eb0c21ef03d887ac
Eda Dursun <edadrsn929@gmail.com>
2025-07-01 09:16:20 +0300

    first page created

---
+++ android/app/src/main/java/com/example/usiapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.usiapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.usiapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
