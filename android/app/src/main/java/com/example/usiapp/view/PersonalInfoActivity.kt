2cbdc997c15222924b6375be5bdafccc2f7664d9
Eda Dursun <edadrsn929@gmail.com>
2025-07-02 13:29:40 +0300

    personal info activity created

--- android/app/src/main/java/com/example/usiapp/view/PersonalInfoActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/PersonalInfoActivity.kt
@@ -1,6 +1,10 @@
 package com.example.usiapp.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +22,18 @@ class PersonalInfoActivity : AppCompatActivity() {
         binding=ActivityPersonalInfoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val unvanlar = listOf("Prof. Dr.", "Doç. Dr.", "Dr. Öğr. Üyesi", "Araş. Gör.", "Öğr. Gör.")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, unvanlar)
+        val dropdown = findViewById<AutoCompleteTextView>(R.id.unvanDropdown)
+        dropdown.setAdapter(adapter)
+        dropdown.setOnClickListener {
+            dropdown.showDropDown()
+        }
 
+    }
+
+    fun goToBack(view: View){
+        val intent= Intent(this,AcademicianActivity::class.java)
+        startActivity(intent)
     }
 }
