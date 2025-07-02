b12a89d4437f2987b9c6d8092ce0e3f9ac44d752
Eda Dursun <edadrsn929@gmail.com>
2025-07-02 13:46:44 +0300

    contact activity and xml created

--- android/app/src/main/java/com/example/usiapp/view/ContactInfoActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/ContactInfoActivity.kt
@@ -1,21 +1,29 @@
 package com.example.usiapp.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.usiapp.R
+import com.example.usiapp.databinding.ActivityContactInfoBinding
 
 class ContactInfoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityContactInfoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_contact_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityContactInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun goToBack(view: View){
+        val intent= Intent(this,AcademicianActivity::class.java)
+        startActivity(intent)
     }
 }
