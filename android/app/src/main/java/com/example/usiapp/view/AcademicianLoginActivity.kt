e078cea98533b64d6a6ae21e695671f93e259b94
Eda Dursun <edadrsn929@gmail.com>
2025-07-04 21:06:02 +0300

    signIn and gotoSignUp functions added

--- android/app/src/main/java/com/example/usiapp/view/AcademicianLoginActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/AcademicianLoginActivity.kt
@@ -13,31 +13,50 @@ import com.example.usiapp.databinding.ActivityAcademicianLoginBinding
 
 class AcademicianLoginActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityAcademicianLoginBinding
+    private lateinit var binding: ActivityAcademicianLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityAcademicianLoginBinding.inflate(layoutInflater)
+        binding = ActivityAcademicianLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }
 
 
-    fun signIn(view: View){
-        val sicilText = binding.sicilNo.text.toString()
-        if(sicilText!=null && sicilText.length==5){
+    fun signIn(view: View) {
+        val academicianMail = binding.academicianMail.text.toString()
+        val academicianPassword = binding.academicianPassword.text.toString()
+        if (academicianMail.contains("@") || academicianPassword.contains("@ahievran.edu.tr")) {
+            if (academicianPassword.length >= 6) {
                 val intent = Intent(this@AcademicianLoginActivity, AcademicianActivity::class.java)
+                intent.putExtra(academicianMail, "academicianMail")
+                intent.putExtra(academicianPassword, "academicianPassword")
                 startActivity(intent)
-        }else{
-            Toast.makeText(this@AcademicianLoginActivity,"📢 Lütfen Sicil Numaranızı girin",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(
+                    this@AcademicianLoginActivity, "📢Şifre en az 6 karakter olmalıdır.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        } else {
+            Toast.makeText(
+                this@AcademicianLoginActivity,
+                "📢Geçersiz mail adresi.Sadece @ahievran.edu.tr uzantılı mail kullanılabilir.",
+                Toast.LENGTH_LONG
+            ).show()
         }
 
 
     }
 
-    fun gotoBack(view:View){
-        val intent=Intent(this@AcademicianLoginActivity,MainActivity::class.java)
+    fun gotoSignUp(view: View) {
+        val intent = Intent(this@AcademicianLoginActivity, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun gotoBack(view: View) {
+        val intent = Intent(this@AcademicianLoginActivity, MainActivity::class.java)
         startActivity(intent)
     }
 }
