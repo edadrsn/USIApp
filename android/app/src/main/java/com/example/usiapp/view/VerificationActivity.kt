c28efd07fdb8ea25637e9871079016300578f76d
Eda Dursun <edadrsn929@gmail.com>
2025-07-07 21:18:18 +0300

    verification codes created

--- android/app/src/main/java/com/example/usiapp/view/VerificationActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/VerificationActivity.kt
@@ -12,44 +12,49 @@ import com.example.usiapp.R
 import com.example.usiapp.databinding.ActivityVerificationBinding
 import com.google.firebase.auth.FirebaseAuth
 
+
 class VerificationActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityVerificationBinding
+    private lateinit var binding: ActivityVerificationBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var btnVerification: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityVerificationBinding.inflate(layoutInflater)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         auth = FirebaseAuth.getInstance()
-        btnVerification = findViewById(R.id.btnVerification)
 
+        // Butona tıklanıldığında mail doğrulamasını kontrol et
+        btnVerification = findViewById(R.id.btnVerification)
         btnVerification.setOnClickListener {
             checkEmailVerification()
         }
     }
 
-
+    // Kullanıcının e-posta adresinin doğrulanıp doğrulanmadığını kontrol et
     private fun checkEmailVerification() {
-        val user = auth.currentUser
+        val user = auth.currentUser // Mevcut kullanıcıyı al
+        // Kullanıcı bilgisini güncelle
         user?.reload()?.addOnCompleteListener { task ->
             if (task.isSuccessful) {
+                // Eğer kullanıcı e-postasını doğrulamışsa
                 if (user.isEmailVerified) {
                     Toast.makeText(this, "E-posta doğrulandı. Giriş yapabilirsiniz.", Toast.LENGTH_LONG).show()
 
-                    // Doğrulama başarılıysa giriş ekranına veya direkt ana sayfaya yönlendir
+                    // Doğrulama başarılıysa
                     val intent = Intent(this, PreviewActivity::class.java)
+
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                     startActivity(intent)
-
                 } else {
+                    // Kullanıcı henüz mail doğrulamamışsa
                     Toast.makeText(this, "E-postanızı henüz doğrulamadınız.", Toast.LENGTH_SHORT).show()
                 }
             } else {
+                // Eğer kullanıcı bilgisi güncellenemezse toast mesajını göster
                 Toast.makeText(this, "Doğrulama kontrolü başarısız oldu.", Toast.LENGTH_SHORT).show()
             }
         }
