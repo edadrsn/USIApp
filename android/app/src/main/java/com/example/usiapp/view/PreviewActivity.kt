e0941ca73cff3c4a6cb153e68ede144a6e083b75
Eda Dursun <edadrsn929@gmail.com>
2025-07-09 11:22:50 +0300

    Preview Activity created

--- android/app/src/main/java/com/example/usiapp/view/PreviewActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/PreviewActivity.kt
@@ -12,29 +12,27 @@ import com.example.usiapp.databinding.ActivityPreviewBinding
 
 class PreviewActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityPreviewBinding
+    private lateinit var binding: ActivityPreviewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-       binding=ActivityPreviewBinding.inflate(layoutInflater)
+        binding = ActivityPreviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
+        
         val bottomNavigation = binding.bottomNavigation
 
-        // Home sekmesi seçili görünsün çünkü bu PreviewActivity
+        // Home sekmesi seçili
         bottomNavigation.selectedItemId = R.id.home
 
         bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
+                // Profile sekmesine basınca AcademicianActivity'e geç
                 R.id.home -> {
-                    // Zaten bu sayfadasın, hiçbir şey yapma
                     true
                 }
-
+                // Profile sekmesine basınca AcademicianActivity'e geç
                 R.id.profile -> {
-                    // Profile sekmesine basınca AcademicianActivity'e geç
                     val intent = Intent(this, AcademicianActivity::class.java)
                     startActivity(intent)
                     finish()
@@ -44,14 +42,5 @@ class PreviewActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
-
-
-
-    }
-
-    fun goToBack(view: View) {
-        val intent = Intent(this@PreviewActivity, AcademicianActivity::class.java)
-        startActivity(intent)
     }
 }
