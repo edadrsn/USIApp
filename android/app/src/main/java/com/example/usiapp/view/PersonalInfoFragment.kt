3e0e2622202d660f47305a1fc28af923e50bad7d
Eda Dursun <edadrsn929@gmail.com>
2025-07-09 14:45:59 +0300

    PersonalInfoActivity page was converted to a fragment structure

---
+++ android/app/src/main/java/com/example/usiapp/view/PersonalInfoFragment.kt
@@ -0,0 +1,54 @@
+package com.example.usiapp.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.usiapp.R
+import com.example.usiapp.databinding.FragmentPersonalInfoBinding
+
+class PersonalInfoFragment : Fragment() {
+
+    private var _binding:FragmentPersonalInfoBinding? = null
+    private val binding get()= _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding=FragmentPersonalInfoBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Kullanıcıya seçtirmek istediğim ünvanlar
+        val unvanlar = listOf(
+            "Profesör Doktor",
+            "Doçent Doktor",
+            "Doktor Öğretim Üyesi",
+            "Araştırma Görevlisi",
+            "Öğretim Görevlisi"
+        )
+
+        // Dropdown için adapter oluşturdum ve basit bir tek satırlı görünüme sahip liste tanımladım
+        val adapter = ArrayAdapter(
+            requireContext(),
+            android.R.layout.simple_dropdown_item_1line,
+            unvanlar
+        )
+
+        val dropdown = binding.unvanDropdown
+        // Adapter dropdown'a atadım
+        dropdown.setAdapter(adapter)
+
+        // Kullanıcı kutuya tıkladığında listeyi göster
+        dropdown.setOnClickListener {
+            dropdown.showDropDown()
+        }
+    }
+
+}
