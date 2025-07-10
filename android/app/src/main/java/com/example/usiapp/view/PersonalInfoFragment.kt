aadb36cfbf5aff5dff7419bd6fd29a5c7491bd2b
Eda Dursun <edadrsn929@gmail.com>
2025-07-10 10:51:35 +0300

    goToBack functions added and updated

--- android/app/src/main/java/com/example/usiapp/view/PersonalInfoFragment.kt
+++ android/app/src/main/java/com/example/usiapp/view/PersonalInfoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.usiapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,14 +12,14 @@ import com.example.usiapp.databinding.FragmentPersonalInfoBinding
 
 class PersonalInfoFragment : Fragment() {
 
-    private var _binding:FragmentPersonalInfoBinding? = null
-    private val binding get()= _binding!!
+    private var _binding: FragmentPersonalInfoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding=FragmentPersonalInfoBinding.inflate(inflater,container,false)
+        _binding = FragmentPersonalInfoBinding.inflate(inflater, container, false)
         return binding.root
     }
 
@@ -49,6 +50,10 @@ class PersonalInfoFragment : Fragment() {
         dropdown.setOnClickListener {
             dropdown.showDropDown()
         }
-    }
 
+        binding.goToBack.setOnClickListener {
+            val intent = Intent(requireContext(), AcademicianActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
