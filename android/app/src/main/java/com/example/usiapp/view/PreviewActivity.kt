0ed5a81374b854f2f9419f66ddf07749d6be676c
Eda Dursun <edadrsn929@gmail.com>
2025-07-11 11:29:30 +0300

    preview activity updated

--- android/app/src/main/java/com/example/usiapp/view/PreviewActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/PreviewActivity.kt
@@ -1,21 +1,45 @@
 package com.example.usiapp.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.usiapp.R
+import com.example.usiapp.databinding.ActivityPreviewBinding
 
 class PreviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPreviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_preview)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityPreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigation = binding.bottomNavigation
+
+        // Home sekmesi seçili
+        bottomNavigation.selectedItemId = R.id.preview
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                // Profile sekmesine basınca AcademicianActivity'e geç
+                R.id.preview -> {
+                    true
+                }
+                // Profile sekmesine basınca AcademicianActivity'e geç
+                R.id.profile -> {
+                    val intent = Intent(this, AcademicianActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
