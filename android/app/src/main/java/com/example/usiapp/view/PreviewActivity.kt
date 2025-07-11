2145c470dfc4434326c25223ae2f4ee7ebd55906
Eda Dursun <edadrsn929@gmail.com>
2025-07-11 11:24:06 +0300

    academician.xml updated

--- android/app/src/main/java/com/example/usiapp/view/PreviewActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/PreviewActivity.kt
@@ -1,46 +1,21 @@
 package com.example.usiapp.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.usiapp.R
-import com.example.usiapp.databinding.ActivityPreviewBinding
 
 class PreviewActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityPreviewBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityPreviewBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val bottomNavigation = binding.bottomNavigation
-
-        // Home sekmesi seçili
-        bottomNavigation.selectedItemId = R.id.home
-
-        bottomNavigation.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                // Profile sekmesine basınca AcademicianActivity'e geç
-                R.id.home -> {
-                    true
-                }
-                // Profile sekmesine basınca AcademicianActivity'e geç
-                R.id.profile -> {
-                    val intent = Intent(this, AcademicianActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                    true
-                }
-
-                else -> false
-            }
+        setContentView(R.layout.activity_preview)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
