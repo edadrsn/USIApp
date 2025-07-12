466b0c459eb7b7c954b2ae7bb155f178e0a9e108
Eda Dursun <edadrsn929@gmail.com>
2025-07-12 15:06:20 +0300

    PersonalInfoFragment degree updated,personName and personSurname make enable

--- android/app/src/main/java/com/example/usiapp/view/PersonalInfoFragment.kt
+++ android/app/src/main/java/com/example/usiapp/view/PersonalInfoFragment.kt
@@ -6,14 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import android.widget.Toast
 import com.example.usiapp.R
 import com.example.usiapp.databinding.FragmentPersonalInfoBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class PersonalInfoFragment : Fragment() {
 
     private var _binding: FragmentPersonalInfoBinding? = null
     private val binding get() = _binding!!
+    private lateinit var personName: EditText
+    private lateinit var personSurname: EditText
+    private lateinit var personDegree: AutoCompleteTextView
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+    private var documentId:String?= null
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,11 +41,13 @@ class PersonalInfoFragment : Fragment() {
 
         // Kullanıcıya seçtirmek istediğim ünvanlar
         val unvanlar = listOf(
-            "Profesör Doktor",
-            "Doçent Doktor",
-            "Doktor Öğretim Üyesi",
-            "Araştırma Görevlisi",
-            "Öğretim Görevlisi"
+            "Prof. Dr.",
+            "Doç. Dr.",
+            "Dr. Öğr. Üyesi",
+            "Dr.",
+            "Öğr. Gör. Dr.",
+            "Öğr. Gör.",
+            "Arş. Gör."
         )
 
         // Dropdown için adapter oluşturdum ve basit bir tek satırlı görünüme sahip liste tanımladım
@@ -42,7 +57,7 @@ class PersonalInfoFragment : Fragment() {
             unvanlar
         )
 
-        val dropdown = binding.unvanDropdown
+        val dropdown = binding.personDegree
         // Adapter dropdown'a atadım
         dropdown.setAdapter(adapter)
 
@@ -51,14 +66,113 @@ class PersonalInfoFragment : Fragment() {
             dropdown.showDropDown()
         }
 
+        //Geri dön
         binding.goToBack.setOnClickListener {
             val intent = Intent(requireContext(), AcademicianActivity::class.java)
             startActivity(intent)
         }
+
+
+        personName = binding.personName
+        personSurname = binding.personSurname
+        personDegree = binding.personDegree
+
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+
+        val currentUserEmail = auth.currentUser?.email
+        if (currentUserEmail != null) {
+            getAcademicianInfo(currentUserEmail)
+        }
+
+
+        binding.btnUpdate.setOnClickListener {
+            updateAcademicianInfo()
+        }
+
+
+
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    //Firebaseden çekilen veriyi ilgili alanlara ata
+    private fun getAcademicianInfo(email: String) {
+        db.collection("AcademicianInfo")
+            .whereEqualTo("Email", email)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    val doc = documents.documents[0]
+                    this.documentId = doc.id
+                    val fullName = doc.getString("adSoyad") ?: ""
+                    val degree = doc.getString("Unvan") ?: ""
+
+                    //Firebaseden çekilen adSoyadı parçala
+                    val nameParts = fullName.trim().split(" ")
+                    if (nameParts.size >= 2) {
+                        val surname = nameParts.last() // Son kelime soyadı
+                        val name = nameParts.subList(0, nameParts.size - 1)
+                            .joinToString(" ") // Boşluğa göre birleştirir
+
+                        personName.setText(name)
+                        personSurname.setText(surname)
+                    } else {
+                        // Tek kelimelik isim varsa her ikisine aynı şeyi koy
+                        personName.setText(fullName)
+                        personSurname.setText("")
+                    }
+
+                    personDegree.setText(degree,false)
+                    binding.personName.isEnabled=false
+                    binding.personSurname.isEnabled=false
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Akademisyen bilgisi bulunamadı !",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(requireContext(), "Hata: ${e.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
     }
-}
+
+    //Update
+    private fun updateAcademicianInfo(){
+        val updateName=binding.personName.text.toString()
+        val updateSurname=binding.personSurname.text.toString()
+        val updateDegree=binding.personDegree.text.toString()
+        if(updateName.isEmpty() || updateSurname.isEmpty() ||updateDegree.isEmpty()){
+            Toast.makeText(requireContext(),"Lütfen tüm alanları doldurun!",Toast.LENGTH_LONG).show()
+            return
+        }
+        val fullName="$updateName $updateSurname"
+        val updates= hashMapOf<String,Any>(
+            "adSoyad" to fullName,
+            "Unvan" to updateDegree
+        )
+        if(documentId == null){
+            Toast.makeText(requireContext(),"Belge bulunamadı,lütfen tekrar deneyiniz !",Toast.LENGTH_LONG).show()
+            return
+        }
+
+        db.collection("AcademicianInfo").document(documentId!!)
+            .update(updates)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(),"Bilgiler başarıyla güncellendi.",Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener { e->
+                Toast.makeText(requireContext(),"Güncelleme başarısız: ${e.localizedMessage}",Toast.LENGTH_LONG).show()
+            }
+
+
+    }
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+
+    }
