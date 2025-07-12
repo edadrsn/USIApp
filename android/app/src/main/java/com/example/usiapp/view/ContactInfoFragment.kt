1fe26fe6bf6745e5090834eadd1f9163a20a7e8a
Eda Dursun <edadrsn929@gmail.com>
2025-07-12 18:22:27 +0300

    data retrieval and updating process was performed

--- android/app/src/main/java/com/example/usiapp/view/ContactInfoFragment.kt
+++ android/app/src/main/java/com/example/usiapp/view/ContactInfoFragment.kt
@@ -1,5 +1,6 @@
 package com.example.usiapp.view
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,9 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import android.widget.Toast
 import com.example.usiapp.R
 import com.example.usiapp.databinding.FragmentContactInfoBinding
 import com.google.common.reflect.TypeToken
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.gson.Gson
 
 
@@ -18,6 +24,18 @@ class ContactInfoFragment : Fragment() {
     private var _binding: FragmentContactInfoBinding? = null
     private val binding get() = _binding!!
     private lateinit var illerVeIlceler: Map<String, List<String>>
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private var documentId: String? = null
+
+    private lateinit var userPhoneNum: EditText
+    private lateinit var userCorporateNum: EditText
+    private lateinit var userEmail: EditText
+    private lateinit var userWebsite: EditText
+    private lateinit var userProvince: AutoCompleteTextView
+    private lateinit var userDistrict: AutoCompleteTextView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,17 +88,141 @@ class ContactInfoFragment : Fragment() {
         }
 
 
+        //Geri dön
         binding.goToBack.setOnClickListener {
             val intent = Intent(requireContext(), AcademicianActivity::class.java)
             startActivity(intent)
         }
 
+
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        userPhoneNum = binding.phoneNumber
+        userCorporateNum = binding.corporateNumber
+        userEmail = binding.email
+        userWebsite = binding.webSite
+        userProvince = binding.province
+        userDistrict = binding.district
+
+        val currentUserEmail = auth.currentUser?.email
+        if (currentUserEmail != null) {
+            getContactInfo(currentUserEmail)
+        }
+
+
+        //Butona basınca güncelle
+        binding.updateContact.setOnClickListener {
+            AlertDialog.Builder(requireContext()).apply {
+                setTitle("Güncelleme")
+                setMessage("Güncellemek istediğinize emin misiniz ?")
+                setPositiveButton("Evet") { dialog, _ ->
+                    updateContactInfo()
+                    dialog.dismiss()
+                }
+                setNegativeButton("Hayır") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                create()
+                show()
+            }
+        }
     }
 
+
+    //Firebaseden verileri çek
+    private fun getContactInfo(email: String) {
+        db.collection("AcademicianInfo")
+            .whereEqualTo("Email", email)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (!documents.isEmpty) {
+                    val doc = documents.documents[0]
+                    this.documentId = doc.id
+                    val getPhone = doc.getString("tel") ?: ""
+                    val getCorporate = doc.getString("kurumsalTel") ?: ""
+                    val getEmail = doc.getString("Email") ?: ""
+                    val getWebsite = doc.getString("web") ?: ""
+                    val getProvince = doc.getString("sehir") ?: ""
+                    val getDistrict = doc.getString("ilce") ?: ""
+
+
+                    userPhoneNum.setText(getPhone)
+                    userCorporateNum.setText(getCorporate)
+                    userEmail.setText(getEmail)
+                    userWebsite.setText(getWebsite)
+                    userProvince.setText(getProvince, false)
+                    userDistrict.setText(getDistrict, false)
+
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(requireContext(), "Hata: ${e.localizedMessage}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+    }
+
+
+    //İletişim bilgilerini güncelle
+    private fun updateContactInfo() {
+        val updatePhone=binding.phoneNumber.text.toString()
+        val updateCorporateNum=binding.corporateNumber.text.toString()
+        val updateEmail=binding.email.text.toString()
+        val updateWebsite=binding.webSite.text.toString()
+        val updateProvince=binding.province.text.toString()
+        val updateDistrict=binding.district.text.toString()
+
+        if(updatePhone.isEmpty()){
+            Toast.makeText(requireContext(),"Telefon alanı boş bırakılamaz!",Toast.LENGTH_LONG).show()
+        }else{
+            if(updateCorporateNum.isEmpty()){
+                Toast.makeText(requireContext(),"Kurumsal telefon alanı boş bırakılamaz!",Toast.LENGTH_LONG).show()
+            }else{
+                if(updateEmail.isEmpty()){
+                    Toast.makeText(requireContext(),"Email boş bırakılamaz!",Toast.LENGTH_LONG).show()
+                }else{
+                    if(updateWebsite.isEmpty()){
+                        Toast.makeText(requireContext(),"Website alanı boş bırakılamaz!",Toast.LENGTH_LONG).show()
+                    }else{
+                        if(updateProvince.isEmpty()){
+                            Toast.makeText(requireContext(),"İl boş bırakılamaz!",Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+        }
+
+        if(documentId == null){
+            Toast.makeText(requireContext(),"Belge bulunamadı,lütfen tekrar deneyiniz !",Toast.LENGTH_LONG).show()
+            return
+        }
+        val updates= hashMapOf<String,Any>(
+            "tel" to updatePhone,
+            "kurumsalTel" to updateCorporateNum,
+            "Email" to updateEmail,
+            "web" to updateWebsite,
+            "sehir" to updateProvince,
+            "ilce" to updateDistrict
+        )
+
+        db.collection("AcademicianInfo").document(documentId!!)
+            .update(updates)
+            .addOnSuccessListener {
+                    Toast.makeText(requireContext(),"Bilgiler başarıyla güncellendi !",Toast.LENGTH_LONG).show()
+            }
+            .addOnFailureListener {e->
+                    Toast.makeText(requireContext(),"Güncelleme başarısız ${e.localizedMessage} !",Toast.LENGTH_LONG).show()
+
+            }
+
+    }
+
+
+    //Json
     private fun loadJsonFromAsset(fileName: String): String {
         val inputStream = requireContext().assets.open(fileName)
         return inputStream.bufferedReader().use { it.readText() }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
