e4aa82f0edfc510cc077df22c7f13f30d9c09b1e
Eda Dursun <edadrsn929@gmail.com>
2025-07-19 19:37:44 +0300

    Application icon size updated, JSON and district data updated, new XML files created, nav menu icon titles removed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.ui.graphics.android)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -64,6 +66,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.google.android.flexbox:flexbox:3.0.0")
+
 
 
 }
