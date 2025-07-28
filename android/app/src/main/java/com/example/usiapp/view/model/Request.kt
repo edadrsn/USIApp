81b7eccda84eeedc6aa3e434205cd28036fefa35
Eda Dursun <edadrsn929@gmail.com>
2025-07-28 14:19:12 +0300

    Request creation completed. Request category message subject and designs completed

--- android/app/src/main/java/com/example/usiapp/view/model/Request.kt
+++ android/app/src/main/java/com/example/usiapp/view/model/Request.kt
@@ -1,3 +1,10 @@
 package com.example.usiapp.view.model
 
-data class Request()
+data class Request(
+    val title: String,
+    val message: String,
+    val date: String,
+    val selectedCategories: List<String>
+)
+
+
