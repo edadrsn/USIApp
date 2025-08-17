e035e493e9834f594bde2ba7cfa49852c9aa40b8
Eda Dursun <edadrsn929@gmail.com>
2025-08-17 22:38:37 +0300

    xml updated

---
+++ android/app/src/main/java/com/example/usiapp/view/industryView/IndustryAddressInfoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.usiapp.view.industryView
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.usiapp.R
+
+class IndustryAddressInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_industry_address_info)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
