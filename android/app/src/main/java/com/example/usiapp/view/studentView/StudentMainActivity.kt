5b9f87dbe2ac17c26f57fd8024133d62a78517bd
Eda Dursun <edadrsn929@gmail.com>
2025-09-04 22:04:31 +0300

    fragment controls made,new json file added,nav menu student and nav graph student xmls created

--- android/app/src/main/java/com/example/usiapp/view/studentView/StudentMainActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/studentView/StudentMainActivity.kt
@@ -3,19 +3,51 @@ package com.example.usiapp.view.studentView
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.usiapp.R
+import com.example.usiapp.databinding.ActivityStudentMainBinding
 
 class StudentMainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStudentMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_student_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityStudentMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNav = binding.bottomNavigationStudent
+
+        // NavController’ı host fragment’tan al
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_student_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // BottomNavigationView ile senkronize et
+        NavigationUI.setupWithNavController(bottomNav, navController)
+
+        // Eğer başka bir sayfadan "intent" ile gelindiyse
+        val goTo = intent.getStringExtra("goToFragment")
+
+        if (savedInstanceState == null) {
+            when (goTo) {
+                "incomingRequestStudent" -> {
+                    bottomNav.selectedItemId = R.id.fragmentIncomingRequestStudent
+                    navController.navigate(R.id.fragmentIncomingRequestStudent)
+                }
+                "createRequestStudent" -> {
+                    bottomNav.selectedItemId = R.id.fragmentCreateRequestStudent
+                    navController.navigate(R.id.fragmentCreateRequestStudent)
+                }
+                else -> {
+                    bottomNav.selectedItemId = R.id.fragmentStudentProfile
+                    navController.navigate(R.id.fragmentStudentProfile)
+                }
+            }
         }
     }
 }
--- android/app/src/main/java/com/example/usiapp/view/studentView/StudentProfileFragment.kt
+++ android/app/src/main/java/com/example/usiapp/view/studentView/StudentProfileFragment.kt
@@ -1,60 +1,297 @@
 package com.example.usiapp.view.studentView
 
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.usiapp.R
+import com.example.usiapp.databinding.FragmentStudentProfileBinding
+import com.example.usiapp.view.academicianView.MainActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
+import java.util.UUID
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StudentProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StudentProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentStudentProfileBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
+
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedBitmap: Bitmap? = null
+    var selectedPicture: Uri? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_profile, container, false)
+        _binding = FragmentStudentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        registerLauncher()
+
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
+
+        val currentUserEmail = auth.currentUser?.email
+
+        //Verileri çek
+        db.collection("Students")
+            .whereEqualTo("studentEmail", currentUserEmail)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    val document = querySnapshot.documents[0]
+                    binding.studentNameTxt.setText(document.getString("studentName") ?: "")
+                    binding.studentEmailTxt.setText(document.getString("studentEmail") ?: "")
+
+                    val getPhoto = document.getString("studentImage")
+                    if (!getPhoto.isNullOrEmpty()) {
+                        Picasso.get()
+                            .load(getPhoto)
+                            .placeholder(R.drawable.person)
+                            .error(R.drawable.person)
+                            .into(binding.studentImage)
+                    }
+                }
+            }
+
+
+        //Kartlara tıklama
+        binding.addImage.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                // Android 13 ve sonrası
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            requireActivity(),
+                            Manifest.permission.READ_MEDIA_IMAGES
+                        )
+                    ) {
+                        Snackbar.make(
+                            view,
+                            "Galeriye erişmek için izin gerekli",
+                            Snackbar.LENGTH_INDEFINITE
+                        ).setAction("İzin Ver") {
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }.show()
+                    } else {
+                        permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                    }
+                } else {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                }
+            } else {
+                // Android 12 ve öncesi
+                if (ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            requireActivity(),
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        )
+                    ) {
+                        Snackbar.make(
+                            view,
+                            "Galeriye erişmek için izin gerekli",
+                            Snackbar.LENGTH_INDEFINITE
+                        ).setAction("İzin Ver") {
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }.show()
+                    } else {
+                        permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    }
+                } else {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                }
+            }
+        }
+
+        binding.cardStudentInfo.setOnClickListener {
+            startActivity(Intent(requireContext(), StudentInfoActivity::class.java))
+        }
+
+        binding.cardUniversityInfo.setOnClickListener {
+            startActivity(Intent(requireContext(), UniversityInfoActivity::class.java))
+        }
+
+        binding.cardStudentDepartmentInfo.setOnClickListener {
+            startActivity(Intent(requireContext(), DepartmentInfoActivity::class.java))
+        }
+
+        binding.cardStudentClassInfo.setOnClickListener {
+            startActivity(Intent(requireContext(), ClassInfoActivity::class.java))
+        }
+
+        binding.logOutStudent.setOnClickListener {
+            startActivity(Intent(requireContext(), MainActivity::class.java))
+            auth.signOut()
+        }
+    }
+
+    // Galeriye gitmek ve izin almak için launcher'ları kaydediyorum
+    private fun registerLauncher() {
+        // Galeriden resim seçtikten sonra ne yapacağımı burada belirtiyorum
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        selectedPicture = intentFromResult.data
+                        try {
+                            // Android 9 ve üstü için ImageDecoder
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                val source = ImageDecoder.createSource(
+                                    requireContext().contentResolver,
+                                    selectedPicture!!
+                                )
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.studentImage.setImageBitmap(selectedBitmap)
+                            } else {
+                                // Android 8 ve altı için MediaStore
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireContext().contentResolver,
+                                    selectedPicture
+                                )
+                                binding.studentImage.setImageBitmap(selectedBitmap)
+                            }
+
+
+                            // Firebase Storage ve Firestore'a yükleme örneği
+                            if (selectedPicture != null) {
+                                val uuid = UUID.randomUUID()
+                                val imageName = "$uuid.jpg"
+
+                                // Storage referansı
+                                val storageRef =
+                                    storage.reference.child("student_images").child(imageName)
+
+                                storageRef.putFile(selectedPicture!!)
+                                    .addOnSuccessListener {
+                                        // URL'yi al ve Firestore'a kaydet
+                                        storageRef.downloadUrl.addOnSuccessListener { uri ->
+                                            val downloadUrl = uri.toString()
+
+                                            // Burada Industry koleksiyonundaki ilgili dokümanı güncelliyoruz
+                                            val currentUserEmail = auth.currentUser?.email
+
+                                            if (currentUserEmail != null) {
+                                                db.collection("Students")
+                                                    .whereEqualTo(
+                                                        "studentEmail",
+                                                        currentUserEmail
+                                                    ) // Kullanıcıya ait firmayı bul
+                                                    .get()
+                                                    .addOnSuccessListener { querySnapshot ->
+                                                        if (!querySnapshot.isEmpty) {
+                                                            val documentId =
+                                                                querySnapshot.documents[0].id
+
+                                                            db.collection("Students")
+                                                                .document(documentId)
+                                                                .update(
+                                                                    "studentImage",
+                                                                    downloadUrl
+                                                                )
+                                                                .addOnSuccessListener {
+                                                                    Toast.makeText(
+                                                                        requireContext(),
+                                                                        "Resim başarıyla güncellendi",
+                                                                        Toast.LENGTH_SHORT
+                                                                    ).show()
+                                                                }
+                                                                .addOnFailureListener { e ->
+                                                                    Toast.makeText(
+                                                                        requireContext(),
+                                                                        "Firestore hata: ${e.localizedMessage}",
+                                                                        Toast.LENGTH_SHORT
+                                                                    ).show()
+                                                                }
+                                                        } else {
+                                                            Toast.makeText(
+                                                                requireContext(),
+                                                                "Firma bulunamadı",
+                                                                Toast.LENGTH_SHORT
+                                                            ).show()
+                                                        }
+                                                    }
+                                                    .addOnFailureListener { e ->
+                                                        Toast.makeText(
+                                                            requireContext(),
+                                                            "Sorgu hatası: ${e.localizedMessage}",
+                                                            Toast.LENGTH_SHORT
+                                                        ).show()
+                                                    }
+                                            }
+                                        }
+                                    }
+                                    .addOnFailureListener { e ->
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "Storage hata: ${e.localizedMessage}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                            }
+
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+
+
+        // İzin alma işlemini burada kontrol ediyorum
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    // İzin verildiyse galeriyi aç
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    // İzin verilmediyse uyarı göster
+                    Toast.makeText(requireContext(), "İzin gerekiyor!", Toast.LENGTH_SHORT).show()
                 }
             }
     }
+
 }
