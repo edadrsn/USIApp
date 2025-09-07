0b18c7367525ea62a5f5e1b6e5c2e6d5f9eaac3d
Eda Dursun <edadrsn929@gmail.com>
2025-09-07 21:42:03 +0300

    The process of selecting the student's request category, creating the request subject and message has been completed.

--- android/app/src/main/java/com/example/usiapp/view/studentView/RequestSubjectStudentActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/studentView/RequestSubjectStudentActivity.kt
@@ -1,21 +1,97 @@
 package com.example.usiapp.view.studentView
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.usiapp.R
+import com.example.usiapp.databinding.ActivityRequestSubjectStudentBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class RequestSubjectStudentActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRequestSubjectStudentBinding
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_request_subject_student)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityRequestSubjectStudentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+        val userId = auth.currentUser?.uid ?: ""
+
+        //Öğrencinin talebini Firebase'e  kaydet
+        binding.btnCreateRequest.setOnClickListener {
+            db.collection("Students").document(userId)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        //Firebasedeki firma verilerini al oku
+                        val ogrAd = document.getString("studentName") ?: ""
+                        val ogrEmail = document.getString("studentEmail") ?: ""
+                        val ogrTelefon = document.getString("studentPhone") ?: ""
+                        val ogrResim = document.getString("studentImage") ?: ""
+
+                        //Sayfaya girilen verileri al
+                        val requestTitle = binding.reqTitle.text.toString()
+                        val requestMessage = binding.reqMessage.text.toString()
+                        val selectedCategory = intent.getStringExtra("selectedCategory")
+                        val currentDate =
+                            SimpleDateFormat("dd.MM.yyyy", Locale.getDefault()).format(
+                                Date()
+                            )
+
+                        // Bilgileri Firestore’a ekle
+                        val categoryInfo = hashMapOf(
+                            "createdDate" to currentDate,
+                            "requestMessage" to requestMessage,
+                            "requestTitle" to requestTitle,
+                            "requesterEmail" to ogrEmail,
+                            "requesterID" to userId,
+                            "requesterName" to ogrAd,
+                            "requesterPhone" to ogrTelefon,
+                            "requestCategory" to selectedCategory,
+                            "requesterImage" to ogrResim,
+                            "status" to "pending",
+                            "requesterType" to "student"
+                        )
+                        db.collection("Requests")
+                            .add(categoryInfo)
+                            .addOnSuccessListener {
+                                Toast.makeText(this, "Talep başarıyla kaydedildi!", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, StudentMainActivity::class.java)
+                                intent.putExtra("goToFragment", "createRequestStudent")
+                                startActivity(intent)
+                                finish()
+                            }
+                            .addOnFailureListener {
+                                Toast.makeText(this, "Hata: ${it.localizedMessage}!", Toast.LENGTH_SHORT).show()
+                            }
+                    } else {
+                        Toast.makeText(this, "Öğrenci bilgisi bulunamadı!", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
         }
+
+    }
+
+    //Önceki sayfaya git
+    fun goToPrev(view: View) {
+        startActivity(
+            Intent(
+                this@RequestSubjectStudentActivity,
+                StudentRequestActivity::class.java
+            )
+        )
     }
 }
--- android/app/src/main/java/com/example/usiapp/view/studentView/StudentRequestActivity.kt
+++ android/app/src/main/java/com/example/usiapp/view/studentView/StudentRequestActivity.kt
@@ -1,21 +1,51 @@
 package com.example.usiapp.view.studentView
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.usiapp.R
+import com.example.usiapp.databinding.ActivityStudentRequestBinding
 
 class StudentRequestActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityStudentRequestBinding
+    private lateinit var radioGroupStudent: RadioGroup
+    private lateinit var selectedCategory: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_student_request)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding=ActivityStudentRequestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        radioGroupStudent = binding.radioGroupStudentCategories
+
+        binding.goRequestSubject.setOnClickListener {
+            val selectedId = radioGroupStudent.checkedRadioButtonId
+
+            if (selectedId != -1) { // -1 = hiçbir şey seçilmemiş
+                val selectedRadioButton = findViewById<RadioButton>(selectedId)
+                selectedCategory = selectedRadioButton.text.toString()
+
+                val intent = Intent(this, RequestSubjectStudentActivity::class.java)
+                intent.putExtra("selectedCategory", selectedCategory)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(this, "Lütfen bir kategori seçiniz", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    //Geri dön
+    fun goToBack(view: View) {
+        val intent = Intent(this, StudentMainActivity::class.java)
+        intent.putExtra("goToFragment", "createRequestStudent")
+        startActivity(intent)
+        finish()
     }
 }
